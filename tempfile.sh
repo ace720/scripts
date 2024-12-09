a9d9534335a736d07024d8115361f97a64e1f985
ace720 <imkadam@hotmail.my>
2024-12-09 09:59:55 +0300

    added some changes

--- tempfile.sh
+++ tempfile.sh
@@ -4,19 +4,20 @@ set -eu -o pipefail
 
 # Variable
 export DIR=$HOME/Downloads/temp/
+TotalContent=$(ls -l $DIR | wc -l)
 
 function cleanDir() {
   #check for empty directory
-  if [[ -d "$DIR" ]] then
-	  if [[ "$(ls -A $DIR)" ]] then
-		  rm -rf $DIR*
-		  echo "Deleted $(ls -l | wc -l) contents successfully"
-	  else
-		  echo "$DIR is empty"
-	  fi
-   else
-	   echo "$DIR does not exists!"
-   fi
+  if [[ -d "$DIR" ]]; then
+    if [[ "$(ls -A $DIR)" ]]; then
+      rm -rf $DIR*
+      echo "Deleted $TotalContent contents successfully"
+    else
+      echo "$DIR is empty"
+    fi
+  else
+    echo "$DIR does not exists!"
+  fi
 }
 
 cleanDir
